ff7d6a630041bb211d382179c61692213fe23c20
Maksym Zavalniuk <mezgoodle@gmail.com>
2026-03-06 14:21:13 +0200

    Migrate for auth user model

--- backend/BikeTripper/settings.py
+++ backend/BikeTripper/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from apps import users
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -112,3 +114,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+AUTH_USER_MODEL = 'users.User'
