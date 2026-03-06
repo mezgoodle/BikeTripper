7a32b817ee5bfc4c1a1f3f4c8ab0b28aaf524ddb
Maksym Zavalniuk <mezgoodle@gmail.com>
2026-03-06 19:37:10 +0200

    Add serializers.py and views.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from apps.core.models import BaseModel
 
-class User(AbstractUser):
+
+class User(AbstractUser, BaseModel):
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = (AllowAny,)
+
+
+class MeView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
